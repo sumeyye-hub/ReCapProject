a7c7ee793a7362481bcc41bcd0d9da52414cef2a
Sümeyye Cinaslı <cinaslisumeyye@gmail.com>
2021-01-31 23:46:06 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,25 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _ıcarDal;
+
+        public CarManager(ICarDal carDal)
+        {
+            _ıcarDal = carDal;
+        }
+
+        public List<Car> GetAll()
+        {
+            return _ıcarDal.GetAll();
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,67 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car> {
+            new Car { Id = 1, BrandId = 1, ColorId = 3, DailyPrice = 300, Description = "Range Rover Sport", ModelYear = 2019 },
+            new Car { Id = 2, BrandId = 7, ColorId = 8, DailyPrice = 200, Description = "Lamborghini Huracan", ModelYear = 2017},
+            new Car { Id = 3, BrandId = 5, ColorId = 5, DailyPrice = 200, Description = "Bugatti Veyron Ettore", ModelYear = 2015},
+            new Car { Id = 4, BrandId = 3, ColorId = 3, DailyPrice = 100, Description = "Lamborghini Egoista", ModelYear = 2014},
+            new Car { Id = 5, BrandId = 2, ColorId = 2, DailyPrice = 100, Description = "Porsche 918 Spyder", ModelYear = 2015}
+        
+            };
+            
+            
+            
+        }
+        
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete;
+            foreach (var c in _cars)
+            {
+                if(car.Description == c.Description)
+                {
+                    carToDelete = c;
+                }
+            }
+
+            carToDelete = _cars.SingleOrDefault(c => c.Description == car.Description);
+
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Description == car.Description);
+            carToUpdate.Id = car.Id;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+        }
+    }
+}
